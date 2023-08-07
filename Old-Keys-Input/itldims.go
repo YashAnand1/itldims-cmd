0d7c471f31738bc575e6706a4b3b37c307381af2
yash-anand-fosteringlinux <136465445+yash-anand-fosteringlinux@users.noreply.github.com>
2023-08-07 10:58:27 +0530

    Update itldims.go

--- Old-Keys-Input/itldims.go
+++ Old-Keys-Input/itldims.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/spf13/cobra"
+	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
 var (
-	rootITLDIMS = &cobra.Command{
+	serverIP string
+
+	itldims = &cobra.Command{
 		Use:   "itldims",
 		Short: "Interact with the etcd API",
 		Long:  "A command-line tool to interact with the etcd API and tell if the connection has been made",
@@ -22,51 +25,72 @@ var (
 			defer response.Body.Close()
 
 			if response.StatusCode == http.StatusOK {
-				fmt.Println("Successfully onnected with API. Interaction with etcd can be done.")
+				fmt.Println("Successfully connected with API. Interaction with etcd can be done.")
 			}
 		},
 	}
 
-	getCmd = &cobra.Command{
+	get = &cobra.Command{
 		Use:   "get",
-		Short: "Displays values of an attribute from a server IP",
-		Long:  "Find the value of a specific attribute from a Server IP",
-
-		Run: func(cmd *cobra.Command, args []string) {
-			server, _ := cmd.Flags().GetString("server")
-			if server == "" || len(args) == 0 {
-				log.Fatal("Enter correct server IP and attribute.")
-			}
-
-			attribute := args[0]
-			serverType := "VM"
-			etcdKey := fmt.Sprintf("/servers/%s/%s/%s", serverType, server, attribute)
-
-			response, err := http.Get("http://localhost:8181" + etcdKey)
-			if err != nil {
-				log.Fatalf("Failed to connect to the etcd API.")
-			}
-			defer response.Body.Close()
-
-			if response.StatusCode == http.StatusOK {
-				body, err := ioutil.ReadAll(response.Body)
-				if err != nil {
-					log.Fatalf("Failed to read response body: %v", err)
-				}
-				fmt.Printf("Attribute value for server IP %s and attribute %s: %s\n", server, attribute, string(body))
-			}
-		},
+		Short: "Gets the key or a range of keys",
+		Run:   getCommandFunc,
 	}
 )
 
+// getCommandFunc executes the "get" command.
+func getCommandFunc(cmd *cobra.Command, args []string) {
+	if len(args) != 1 {
+		fmt.Println("get command needs one argument as key")
+		os.Exit(1)
+	}
+
+	key := fmt.Sprintf("/servers/VM/%s/%s", serverIP, args[0])
+	opts := getGetOp(args)
+
+	client, err := clientv3.New(clientv3.Config{
+		Endpoints: []string{"http://localhost:2379"},
+	})
+	if err != nil {
+		fmt.Printf("Failed to initialize etcd client: %v\n", err)
+		os.Exit(1)
+	}
+	defer client.Close()
+
+	ctx := cmd.Context()
+	resp, err := client.Get(ctx, key, opts...)
+	if err != nil {
+		fmt.Printf("Failed to retrieve data from etcd: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Print the retrieved key-values
+	for _, kv := range resp.Kvs {
+		fmt.Printf("Key: %s, Value: %s\n", string(kv.Key), string(kv.Value))
+	}
+}
+
+func getGetOp(args []string) []clientv3.OpOption {
+	if len(args) == 0 {
+		fmt.Println("get command needs one argument as key")
+		os.Exit(1)
+	}
+
+	var opts []clientv3.OpOption
+	if len(args) > 1 {
+		opts = append(opts, clientv3.WithRange(args[1]))
+	}
+
+	return opts
+}
+
 func init() {
-	rootITLDIMS.AddCommand(getCmd)
-	getCmd.Flags().String("server", "", "Server IP to fetch the attribute value from")
-	getCmd.MarkFlagRequired("server")
+	get.Flags().StringVar(&serverIP, "server", "", "Server IP")
+	get.MarkFlagRequired("server")
+	itldims.AddCommand(get)
 }
 
 func main() {
-	if err := rootITLDIMS.Execute(); err != nil {
+	if err := itldims.Execute(); err != nil {
 		log.Fatal(err)
 	}
 }
