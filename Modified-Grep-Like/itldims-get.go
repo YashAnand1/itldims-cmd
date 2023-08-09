a982518f933b3c7517d0c1463a32973017eb9022
yash-anand-fosteringlinux <136465445+yash-anand-fosteringlinux@users.noreply.github.com>
2023-08-10 02:10:45 +0530

    Update itldims-get.go

--- Modified-Grep-Like/itldims-get.go
+++ Modified-Grep-Like/itldims-get.go
@@ -11,12 +11,10 @@ import (
 )
 
 var (
-	onlyKeys bool
-
-	itldims = &cobra.Command{
+	rootCmd = &cobra.Command{
 		Use:   "itldims",
 		Short: "Interact with the etcd API",
-		Long:  "A command-line tool to interact with the etcd API and tell if the connection has been made",
+		Long:  "A command-line tool to interact with the etcd API and check connection",
 		Run: func(cmd *cobra.Command, args []string) {
 			response, err := http.Get("http://localhost:8181/servers/")
 			if err != nil {
@@ -25,81 +23,92 @@ var (
 			defer response.Body.Close()
 
 			if response.StatusCode == http.StatusOK {
-				fmt.Println("Successfully connected with API. Interaction with etcd can be done.")
+				fmt.Println("Connected to API. Interaction with etcd can be done.")
 			}
 		},
 	}
 
-	get = &cobra.Command{
-		Use:   "get",
-		Short: "Search Attributes & Values from etcd API",
-		Long: `Data retrieval can be made possible 'itldims get <input1> <input2>' or 'itldims get <input1>'.
+	keyOnly bool
+)
 
-The possible command combinations that can be used are:
-- itldims get <Servers> | Displays attribute values of all Servers
-- itldims get <Value> | Displays all Servers containing a specific Attribute value
-- itldims get <Attribute> | Displays Displays all Servers containing a specific Attribute
-- itldims get <Server IP> | Displays all Attribute values of a specific Server IP
-- itldims get <Server Type> <Attribute> | Displays specific Attribute values of a specific Server Type
-- itldims get <Server Type> <Value> | Displays all Server Types containing a specific value
-- itldims get <Value> <Server Type> | Displays all Server Types containing a specific value
-- itldims get <Attribute> <Server IP> | Displays specific Attribute values of a specific Server IP
-- itldims get <Server IP> <Attribute> | Displays specific Attribute values of a specific Server IP
-- itldims get <Server IP> <Value> | Displays all Server IPs containing a specific value
-- itldims get <Value> <Server IP> | Displays all Server IPs containing a specific value
-- itldims get <Server IP> <Server Type> | Displays all Attribute values of a specific Server
-		`,
-		Args: cobra.RangeArgs(1, 2),
-		Run: func(cmd *cobra.Command, args []string) {
-			data, err := fetchDataFromEtcdAPI()
-			if err != nil {
-				log.Fatalf("Failed to fetch data from the etcd API: %v", err)
-			}
+var getCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Search Attributes & Values from etcd API",
+	Long: `Data retrieval: 'itldims get <input1> <input2>' or 'itldims get <input1>'.
 
-			if len(args) == 1 {
-				args = append(args, "servers")
-			}
+Commands:
+- itldims get <Servers>          		| Display attributes of all Servers
+- itldims get <Value>            		| Display Servers with a specific Attribute value
+- itldims get <Attribute>        		| Display Servers with a specific Attribute
+- itldims get <Server IP>        		| Display Attribute values of a specific Server IP
+- itldims get <Server Type> <Attribute> | Display specific Attribute values of a Server Type
+- itldims get <Server Type> <Value>     | Display Server Types containing a specific value
+- itldims get <Value> <Server Type>     | Display Server Types containing a specific value
+- itldims get <Attribute> <Server IP>   | Display specific Attribute values of a Server IP
+- itldims get <Server IP> <Attribute>   | Display specific Attribute values of a Server IP
+- itldims get <Server IP> <Value>       | Display Server IPs containing a specific value
+- itldims get <Value> <Server IP>       | Display Server IPs containing a specific value
+- itldims get <Server IP> <Server Type> | Display Attribute values of a specific Server
+	`,
+	Args: cobra.RangeArgs(1, 2),
+	Run: func(cmd *cobra.Command, args []string) {
+		data, err := fetchDataFromAPI()
+		if err != nil {
+			log.Fatalf("Failed to fetch data from the etcd API: %v", err)
+		}
 
-			for key, value := range data {
-				if strings.Contains(key, "{") || strings.Contains(key, "}") ||
-					strings.Contains(value, "{") || strings.Contains(value, "}") {
+		if len(args) == 1 {
+			args = append(args, "servers")
+		}
+
+		for key, value := range parseKeyValuePairs(data) {
+			if strings.Contains(key, args[0]) || strings.Contains(value, args[0]) {
+				if len(args) > 1 && !strings.Contains(key, args[1]) && !strings.Contains(value, args[1]) {
 					continue
 				}
 
-				if !strings.Contains(key, "data") &&
-					(strings.Contains(key, args[0]) || strings.Contains(value, args[0])) &&
-					(strings.Contains(key, args[1]) || strings.Contains(value, args[1])) {
-					fmt.Printf("key=%s\n", key)
-
-					lines := strings.Split(value, "\n")
-					for _, line := range lines {
-						fmt.Println(line)
-					}
-					fmt.Println()
+				if shouldIgnore(key, value) {
+					continue
 				}
-			}
-		},
-	}
-)
 
-func fetchDataFromEtcdAPI() (map[string]string, error) {
+				fmt.Println(key)
+				if !keyOnly {
+					fmt.Println(value)
+				}
+
+				fmt.Println()
+			}
+		}
+	},
+}
+
+func fetchDataFromAPI() (string, error) {
 	response, err := http.Get("http://localhost:8181/servers/")
 	if err != nil {
-		return nil, fmt.Errorf("failed to connect to the etcd API: %v", err)
+		return "", err
 	}
-
 	defer response.Body.Close()
 
 	if response.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("failed to fetch data from the etcd API. Status code: %d", response.StatusCode)
+		return "", fmt.Errorf("failed to fetch data. Status code: %d", response.StatusCode)
 	}
 
-	body, err := io.ReadAll(response.Body)
+	data, err := io.ReadAll(response.Body)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read response body: %v", err)
+		return "", err
 	}
 
-	return parseKeyValuePairs(string(body)), nil
+	return string(data), nil
+}
+
+func shouldIgnore(key, value string) bool {
+	ignoreKeywords := []string{"data", "{", "}"}
+	for _, keyword := range ignoreKeywords {
+		if strings.Contains(key, keyword) || strings.Contains(value, keyword) {
+			return true
+		}
+	}
+	return false
 }
 
 func parseKeyValuePairs(data string) map[string]string {
@@ -120,17 +129,16 @@ func parseKeyValuePairs(data string) map[string]string {
 			result[key] = value
 		}
 	}
-
 	return result
 }
 
 func init() {
-	get.PersistentFlags().BoolVar(&onlyKeys, "only", false, "Display only the keys without their values")
-	itldims.AddCommand(get)
+	rootCmd.AddCommand(getCmd)
+	getCmd.Flags().BoolVar(&keyOnly, "listall", false, "Display only keys without values")
 }
 
 func main() {
-	if err := itldims.Execute(); err != nil {
+	if err := rootCmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
 }
