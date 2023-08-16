609ce3df6129911b1ad9aaf3b1190f5c46079e95
yash-anand-fosteringlinux <136465445+yash-anand-fosteringlinux@users.noreply.github.com>
2023-08-16 17:08:12 +0530

    Update get.go

--- itldims/cmd/get.go
+++ itldims/cmd/get.go
@@ -11,53 +11,74 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// For itldims get [IP/TYPE/ATTRIBUTE/VALUE] [IP/TYPE/ATTRIBUTE/VALUE]
-// IP ATTRIBUTE, IP TYPE, TYPE ATTRIBUTE the kind
-var get = &cobra.Command{
-	Use:   "get",
-	Short: "Search Attributes & Values from etcd API",
-	Long: `Data retrieval can be done using the following combinations:
- - 'itldims get <KeyComponent> <KeyComponent/Value>'
- - 'itldims get <KeyComponent/Value>'
-`,
-	Args: cobra.RangeArgs(1, 2),
+var servers = &cobra.Command{
+	Use:   "servers",
+	Short: "Displays all the running Servers with their Server IPs",
 	Run: func(cmd *cobra.Command, args []string) {
 		data, err := fetchDataFromAPI()
 		if err != nil {
 			log.Fatalf("Failed to fetch data from the etcd API: %v", err)
 		}
 
-		for key, value := range parseKeyValuePairs(data) {
-			if strings.Contains(key, "{") || strings.Contains(key, "}") || strings.Contains(key, "data") ||
-				strings.Contains(value, "{") || strings.Contains(value, "}") {
-				continue
-			}
+		IPs := make(map[string]string)
 
-			ATs := make(map[string]string)
+		for key := range parseKeyValuePairs(data) {
 			splitKey := strings.Split(key, "/")
-			serverAtr := splitKey[4]
 			serverIP := splitKey[3]
-			ATs[serverAtr] = serverAtr
+			IPs[serverIP] = serverIP
+		}
 
-			if strings.Contains(args[0], ".") && strings.Contains(key, args[0]) || strings.Contains(value, args[0]) {
-				if len(args) > 1 && !strings.Contains(key, args[1]) && !strings.Contains(value, args[1]) {
-					continue
-				}
-				fmt.Printf("%s:%s\n", serverAtr, value)
-			} else {
-				if strings.Contains(key, args[0]) || strings.Contains(value, args[0]) {
-					if len(args) > 1 && !strings.Contains(key, args[1]) && !strings.Contains(value, args[1]) {
-						continue
-					}
-					fmt.Printf("Server IP: %s\n%s:%s\n", serverIP, serverAtr, value)
-				}
-			}
+		for serverIP := range IPs {
+			fmt.Printf("%s\n", serverIP)
+			fmt.Printf("----------------------------\n")
 		}
 	},
 }
 
-func init() {
-	get.AddCommand(attributes)
-	get.AddCommand(types)
-	get.AddCommand(servers)
+var types = &cobra.Command{
+	Use:   "types",
+	Short: "Displays all Attribute values of a specific Server Type",
+	Run: func(cmd *cobra.Command, args []string) {
+		data, err := fetchDataFromAPI()
+		if err != nil {
+			log.Fatalf("Failed to fetch data from the etcd API: %v", err)
+		}
+
+		STs := make(map[string]string)
+
+		for key := range parseKeyValuePairs(data) {
+			splitKey := strings.Split(key, "/")
+			serverType := splitKey[2]
+			STs[serverType] = serverType
+		}
+
+		for serverType := range STs {
+			fmt.Printf("%s\n", serverType)
+			fmt.Printf("----------------------------\n")
+		}
+	},
+}
+
+var attributes = &cobra.Command{
+	Use:   "attributes",
+	Short: "Display Servers with a specific Attribute",
+	Run: func(cmd *cobra.Command, args []string) {
+		data, err := fetchDataFromAPI()
+		if err != nil {
+			log.Fatalf("Failed to fetch data from the etcd API: %v", err)
+		}
+
+		ATs := make(map[string]string)
+
+		for key := range parseKeyValuePairs(data) {
+			splitKey := strings.Split(key, "/")
+			serverAtr := splitKey[4]
+			ATs[serverAtr] = serverAtr
+		}
+
+		for serverAtr := range ATs {
+			fmt.Printf("%s\n", serverAtr)
+			fmt.Printf("----------------------------\n")
+		}
+	},
 }
