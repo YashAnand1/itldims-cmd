da33467797ce976c65e6496c2f129b9a1d7aa6ab
yash-anand-fosteringlinux <136465445+yash-anand-fosteringlinux@users.noreply.github.com>
2023-08-18 18:23:00 +0530

    Update get.go

--- itldims/cmd/get.go
+++ itldims/cmd/get.go
@@ -1,63 +1,62 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
 	"fmt"
-	"log"
 	"strings"
 
 	"github.com/spf13/cobra"
 )
 
-// For itldims get [IP/TYPE/ATTRIBUTE] [IP/TYPE/ATTRIBUTE/VALUE]
-// IP ATTRIBUTE, IP TYPE, TYPE ATTRIBUTE the kind
-var get = &cobra.Command{
+var get = &cobra.Command{ // Cobra command is 'get'
 	Use:   "get",
 	Short: "Search Attributes & Values from etcd API",
 	Long: `Data retrieval can be done using the following combinations:
  - 'itldims get <KeyComponent> <KeyComponent/Value>'
  - 'itldims get <KeyComponent/Value>'
 `,
-	Args: cobra.RangeArgs(1, 2),
+	Args: cobra.RangeArgs(1, 2), //number of allowed arguments=1&2
 	Run: func(cmd *cobra.Command, args []string) {
+
 		data, err := fetchDataFromAPI()
 		if err != nil {
-			log.Fatalf("Failed to fetch data from the etcd API: %v", err)
+			fmt.Printf("Failed to fetch data from the etcd API: %v", err) // Print an error message if error occured
 		}
 
-		for key, value := range parseKeyValuePairs(data) {
+		for key, value := range parseKeyValuePairs(data) { // data is gone through and key value variables entered
+
+			// For skipping/continuing data key completely
 			if strings.Contains(key, "{") || strings.Contains(key, "}") || strings.Contains(key, "data") ||
 				strings.Contains(value, "{") || strings.Contains(value, "}") {
 				continue
 			}
 
-			ATs := make(map[string]string)
+			ATs := make(map[string]string) // a MAP for attributes created for storing server attributes
 			splitKey := strings.Split(key, "/")
 			serverAtr := splitKey[4]
 			serverIP := splitKey[3]
-			ATs[serverAtr] = serverAtr
+			ATs[serverAtr] = serverAtr // Store the server attribute in the map
 
-			if strings.Contains(args[0], ".") && strings.Contains(key, args[0]) || strings.Contains(value, args[0]) {
+			// IF SERVERIP IS MENTIONED
+			if strings.Contains(args[0], ".") && (strings.Contains(key, args[0]) || strings.Contains(value, args[0])) {
 				if len(args) > 1 && !strings.Contains(key, args[1]) && !strings.Contains(value, args[1]) {
 					continue
 				}
 				fmt.Printf("%s:\n%s\n\n", serverAtr, value)
-			} else {
+			} else { // IF SERVERIP IS NOT MENTIONED
 				if strings.Contains(key, args[0]) || strings.Contains(value, args[0]) {
 					if len(args) > 1 && !strings.Contains(key, args[1]) && !strings.Contains(value, args[1]) {
 						continue
 					}
-					fmt.Printf("Server IP: %s\n%s:%s\n\n", serverIP, serverAtr, value)
+					fmt.Printf("Server IP:\n%s%s:%s\n\n", serverIP, serverAtr, value) // Print server IP, attribute, and value
 				}
 			}
 		}
 	},
 }
 
+// Initialize 'get' and add subcommands
 func init() {
 	get.AddCommand(attributes)
-	get.AddCommand(types)
+	get.AddCommand(Types)
 	get.AddCommand(servers)
 }
